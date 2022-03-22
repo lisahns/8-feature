485fb739a4b045ecb4e45a330f528943dd60d71d
Lisa Hanes <Lisa.Hanes@hotmail.de>
2022-03-22 16:11:22 +0000

    Javascript - finishing the carousel

--- apps.js
+++ apps.js
@@ -21,23 +21,44 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
     targetSlide.classList.add("current-slide");
 }
 
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove("current-slide");
+targetDot.classList.add("current-slide");
+}
+
 //when click left, move slides to left
 previousButton.addEventListener("click", e => {
     const currentSlide = track.querySelector(".current-slide");
     const previousSlide = currentSlide.previousElementSibling;
-
+    const currentDot = dotNav.querySelector(".current-slide");
+    const previousDot = currentDot.previousElementSibling;
     moveToSlide(track, currentSlide, previousSlide);
+    updateDots(currentDot, previousDot);
 });
 
 //when click right, move slides to right
 nextButton.addEventListener("click", e => {
     const currentSlide = track.querySelector(".current-slide");
-    //console.log(currentSlide);
     const nextSlide = currentSlide.nextElementSibling;
-    //console.log(nextSlide);
+    const currentDot = dotNav.querySelector(".current-slide");
+    const nextDot = currentDot.nextElementSibling;
     moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
 });
 
 
 //when click nav-indicator move to that slide
 
+dotNav.addEventListener("click", e => {
+    //what dot was clicked on?
+const targetDot = e.target.closest("button");
+if (!targetDot) return;
+const currentSlide = track.querySelector(".current-slide");
+const currentDot = dotNav.querySelector(".current-slide");
+const targetIndex = dots.findIndex(dot => dot === targetDot);
+const targetSlide = slides [targetIndex];
+
+moveToSlide(track, currentSlide, targetSlide);
+
+updateDots(currentDot, targetDot);
+});
